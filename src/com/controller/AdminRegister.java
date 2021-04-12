67217f04413570066421d9a8ec0b4a57f6941845
abijotgupta1999 <60977220+abijotgupta@users.noreply.github.com>
2021-04-12 18:49:08 +0530

    Update AdminRegister.java

--- src/com/controller/AdminRegister.java
+++ src/com/controller/AdminRegister.java
@@ -6,25 +6,19 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.bean.AdminBean;
 import com.dao.AdminDaoImpl;
 
 public class AdminRegister extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-   
-    public AdminRegister() {
-        super();
-   
-    }
+  	public AdminRegister() {
+        	super();
+    	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
-
-	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		response.setContentType("text/html");
 		String userName = request.getParameter("name");
 		Long mobileNo = Long.parseLong(request.getParameter("number"));
@@ -38,27 +32,18 @@ public class AdminRegister extends HttpServlet {
 		String status = dao.registerAdmin(admin);
 		
 		RequestDispatcher rd;
-		
-		
 		if(status.equals("Admin Registered Successfully!"))
 		{
 			rd = request.getRequestDispatcher("adminLogin.jsp");
 			request.setAttribute("message", status);
 			rd.forward(request, response);
 		}
-		
-
 		if(status.equals("Email Id Already Registered!") && status.equals("Admin Registration Failed!"))
 		{
 			rd = request.getRequestDispatcher("adminSignup.jsp");
 			request.setAttribute("message", status);
 			rd.forward(request, response);
 		}
-			
-		
-		
-	
 		doGet(request, response);
 	}
-
 }
