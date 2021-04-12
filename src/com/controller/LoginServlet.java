2ca22d87ef534ffc35d93265d07eb665e5eb4783
abijotgupta1999 <60977220+abijotgupta@users.noreply.github.com>
2021-04-12 18:54:30 +0530

    Update LoginServlet.java

--- src/com/controller/LoginServlet.java
+++ src/com/controller/LoginServlet.java
@@ -14,54 +14,28 @@ import com.dao.UserDaoImpl;
 
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-   
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
 		String userName = request.getParameter("userName");
 		String password = request.getParameter("pass");
-		//String userType = request.getParameter("usertype");
-		
 		PrintWriter pw = response.getWriter();
-		
 		response.setContentType("text/html");
-		
 		String status = "Login Denied! Invalid Username or password.";		
-	
 		UserDaoImpl udao = new UserDaoImpl();
-		
 		status = udao.isValidCredential(userName, password);
-			 
 		if(status.equalsIgnoreCase("valid")) {
-				 //valid user
-				 
 			UserBean user = udao.getUserDetails(userName, password);
-				 
 			HttpSession session = request.getSession();
 			session.setAttribute("userdata", user);
 			session.setAttribute("userName", userName);
 			session.setAttribute("password", password);
-			// session.setAttribute("usertype", userType);
-				 
-				 
 			RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
-				 
-			rd.forward(request, response);
-				 
+			rd.forward(request, response);		 
 		}
-			 else {
-				 //invalid user;
-				 
-				RequestDispatcher rd = request.getRequestDispatcher("signin1.jsp");
-					
-				rd.include(request, response);
-				
-				pw.println("<script>document.getElementById('message').innerHTML='"+status+"'</script>");
-				 
-				 
-			 }
-			
-		
-		
+		else {
+			RequestDispatcher rd = request.getRequestDispatcher("signin1.jsp");	
+			rd.include(request, response);
+			pw.println("<script>document.getElementById('message').innerHTML='"+status+"'</script>");
+		 }	
 	}
 }
