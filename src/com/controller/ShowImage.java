905af5773294c6053dfcd57f95d029abfb753933
abijotgupta1999 <60977220+abijotgupta@users.noreply.github.com>
2021-04-12 18:57:14 +0530

    Update ShowImage.java

--- src/com/controller/ShowImage.java
+++ src/com/controller/ShowImage.java
@@ -5,30 +5,21 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.dao.ProductDaoImpl;
-
 
 public class ShowImage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
       
-    public ShowImage() {
-        super();
-    }
-
+    	public ShowImage() {
+        	super();
+    	}
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		String prodId = request.getParameter("id");
 		ProductDaoImpl dao = new ProductDaoImpl();
 		byte[] image = dao.getImage(prodId);
-
 		response.getOutputStream().write(image);
-		
 	}
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
 		doGet(request, response);
 	}
-
 }
