8426a91199415546b26d908f7b08f764bfeea644
abijotgupta1999 <60977220+abijotgupta@users.noreply.github.com>
2021-04-13 17:37:00 +0530

    Update MyCon.java

--- src/com/utility/MyCon.java
+++ src/com/utility/MyCon.java
@@ -5,13 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MyCon {
-	
-
 	public static  Connection dbcon() throws ClassNotFoundException, SQLException {
-		
 		Connection con=(Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/marketplace","root","xxxxxxxx");
-	
-	return con;
-
+		return con;
 	}
 }
