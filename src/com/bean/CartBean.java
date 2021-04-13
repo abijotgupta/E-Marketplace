3f01caf0873869551516c4fc0ed89791374998f7
abijotgupta1999 <60977220+abijotgupta@users.noreply.github.com>
2021-04-13 17:38:51 +0530

    Update CartBean.java

--- src/com/bean/CartBean.java
+++ src/com/bean/CartBean.java
@@ -5,9 +5,7 @@ import java.io.Serializable;
 public class CartBean implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-
 	public CartBean() {}
-	
 	public String userId;
 	public String prodId;
 	public int quantity;
@@ -15,34 +13,25 @@ public class CartBean implements Serializable{
 	public String getUserId() {
 		return userId;
 	}
-
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
-
 	public String getProdId() {
 		return prodId;
 	}
-
 	public void setProdId(String prodId) {
 		this.prodId = prodId;
 	}
-
 	public int getQuantity() {
 		return quantity;
 	}
-
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
-
 	public CartBean(String userId, String prodId, int quantity) {
 		super();
 		this.userId = userId;
 		this.prodId = prodId;
 		this.quantity = quantity;
-	}
-	
-	
-	
+	}	
 }
